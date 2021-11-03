b4d21e7e023ff1194caba4f6b5cb81ef99cd0cf7
webloglife <k.ecchie@gmail.com>
2021-11-03 17:58:57 +0900

    setName by button click

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -74,12 +74,15 @@ export default function SignIn({ setName }) {
           />
           
           <Button
-            type="submit"
+            type="button"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
             disabled={disabled}
+            onClick={() => {
+                setName(string);
+            }}
           >
             はじめる
           </Button>
